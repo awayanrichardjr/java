ba54201840a580a3705f972f2ed0fe6afcf194a9
Richard <awayanrichardjr@gmail.com>
2025-06-01 05:23:23 +0000

    Refactor main method to include additional greeting messages

--- DemoProject/src/App.java
+++ DemoProject/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        String x = "Hello, World!";
+        System.out.println(x);
+
+        String x1 = "Hello", y = "World!";
+        System.out.println(x1 + ", " + y);
     }
 }
