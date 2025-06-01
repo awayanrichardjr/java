ab18d829cdc6e79b16752948e6c7e93ce39eb0be
Richard <awayanrichardjr@gmail.com>
2025-06-01 07:14:30 +0000

    Add initial implementation of remove.java with ArrayList operations

---
+++ ArrayList/src/remove.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+
+
+public class remove {
+    public static void main(String[] args) {
+        ArrayList<String> fruit = new ArrayList<>();
+
+        // add
+        fruit.add("apple");
+        fruit.add("banana");
+        fruit.add("guava");
+        fruit.add("pineapple");
+        fruit.add("durian");
+
+        // remove index 
+        fruit.remove(0);
+        
+        // remove Object
+        fruit.remove("guava");
+
+        // remove All
+        fruit.removeAll(fruit);
+        
+        // clear fruits
+        fruit.clear();
+
+        // remove if...
+        fruit.removeIf(name -> name.startsWith("G"));
+
+        // System.out.println(fruit);
+    }    
+}
